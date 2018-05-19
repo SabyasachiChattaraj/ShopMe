a9c798457c2846b30767d00e9d640de9bc1510de
Sabyasachi Chattaraj <sabyasachi.chattaraj@cognizant.com>
2018-05-19 21:22:33 +0530

    basic struct

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,40 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { DxButtonModule, DxScrollViewModule, DxFormModule, DxTileViewModule } from 'devextreme-angular';
+import { DxButtonModule, DxScrollViewModule, DxFormModule, DxTileViewModule, DxAutocompleteModule } from 'devextreme-angular';
 import { AppComponent } from './app.component';
+import { HeaderComponentComponent } from './header-component/header-component.component';
+import { FooterComponentComponent } from './footer-component/footer-component.component';
+import { ProductsComponent } from './products/products.component';
+import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { ProductDetailComponent } from './product-detail/product-detail.component';
+
+const appRoutes: Routes = [
+  { path: 'Products', component: ProductsComponent },
+  { path: 'Login', component: LoginComponent },
+  { path: '',
+  redirectTo: '/Products',
+  pathMatch: 'full'
+},
+{ path: '**', component: ProductsComponent }
+];
+
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponentComponent,
+    FooterComponentComponent,
+    ProductsComponent,
+    LoginComponent,
+    ProductDetailComponent
   ],
   imports: [
     BrowserModule,
-    DxScrollViewModule, DxFormModule,DxTileViewModule
+    DxScrollViewModule, DxFormModule,DxTileViewModule,DxAutocompleteModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/footer-component/footer-component.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'smapp-footer-component',
+  templateUrl: './footer-component.component.html',
+  styleUrls: ['./footer-component.component.css']
+})
+export class FooterComponentComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
